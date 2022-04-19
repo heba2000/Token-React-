5e07a239506832e4a27976c646d63738dd313814
heba2000 <hebamamdouh15@yahoo.com>
2022-04-20 01:04:48 +0200

    "Login Design setup"

--- src/Components/Login/loginComponent.js
+++ src/Components/Login/loginComponent.js
@@ -1,17 +1,31 @@
 // import {useFormik} from 'formik'
 
-import './login.module.css'
+import './login.css'
 
 
 function Login (){
     return(
-        <section className='login-page'>
-        <div className='container'>
-            <div className='row'>
-                <div className='col-md-6'> 
-                <h1> Hello World</h1>
+        <section className='loginPage'>
+        <div className='container h-100'>
+            <div className='row form-row'>
+                <div className='col-md-5'> 
+                    <form>
+                        <div className="mb-3">
+                            <label for="exampleInputEmail1" className="form-label">Email address</label>
+                            <input type="email" className="form-control"/>
+                        </div>
+                        <div className="mb-3">
+                            <label for="exampleInputPassword1" className="form-label">Password</label>
+                            <input type="password" className="form-control"  />
+                        </div>
+                        <button type="submit" className="btn btn-primary my-2 px-4">Sign In</button>
+                    </form>
                 </div>
-                <div className='col-md-6'> </div>
+                <div className='col-md-7'>
+                    <div className="gaurd-icon">
+                        
+                    </div>
+                </div>
             </div>
         </div>
     </section>
