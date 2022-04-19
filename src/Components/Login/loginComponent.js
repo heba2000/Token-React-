262de53bc69d5e6d9e515451d19be42a85773c75
mheshamm <mheshamm96@gmail.com>
2022-04-19 15:24:37 +0200

    "Background Image added"

---
+++ src/Components/Login/loginComponent.js
@@ -0,0 +1,21 @@
+// import {useFormik} from 'formik'
+
+import './login.module.css'
+
+
+function Login (){
+    return(
+        <section className='login-page'>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-md-6'> 
+                <h1> Hello World</h1>
+                </div>
+                <div className='col-md-6'> </div>
+            </div>
+        </div>
+    </section>
+    )
+}
+
+export default Login ;
